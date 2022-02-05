e557e09c4f6d977872895707344dc935e6802221
Carlos Martinez <Carlos.martdelgado@gmail.com>
2022-02-05 23:56:33 +0100

    First Step3

---
+++ SpeedCar1_files/main.js
@@ -0,0 +1,9 @@
+// * GLOBAL VARIABLES
+
+let splashScreen = document.querySelector("#pantall-inicio");
+let gameOverScreen = document.querySelector("#pantall-reinicio");
+let canvas = document.querySelector("#my-canvas");
+// crear el pincel
+let ctx = canvas.getContext("2d");
+let newGame;
+
