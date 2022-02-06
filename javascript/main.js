2eeda3e6abfcf667b5c908946d2d4eaa568ec3a4
Carlos Martinez <Carlos.martdelgado@gmail.com>
2022-02-06 12:06:49 +0100

    First Step4

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,3 @@
-// * GLOBAL VARIABLES
 
-let splashScreen = document.querySelector("#pantall-inicio");
-let gameOverScreen = document.querySelector("#pantall-reinicio");
-let canvas = document.querySelector("#my-canvas");
-// crear el pincel
-let ctx = canvas.getContext("2d");
-let newGame;
+
 
